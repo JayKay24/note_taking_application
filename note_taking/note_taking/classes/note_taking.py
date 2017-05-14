40cd7470e389e6abc242a86e755a16ea632630f1
James Kinyua <jameskinyua590@gmail.com>
2017-05-14 13:30:40 +0300

    Add methods to class NoteTaking

--- note_taking/note_taking/classes/note_taking.py
+++ note_taking/note_taking/classes/note_taking.py
@@ -16,6 +16,79 @@ from db import NoteDB
 class NoteTaking:
     def __init__(self):
         self.db = NoteDB()
+        self.notes = None
+        self.s_notes = None
         
     def create_note(self, content):
+        """
+        Create a note.
+        """
         note = Note(content)
+        self.db.add_note(note)
+        
+    def view_note(self, note_id):
+        """
+        View contents of a note.
+        """
+        notes = self.db.get_notes()
+        for note in notes:
+            if note_id == note.id:
+                print(note.content)
+        else:
+            print("No note matches that id.")
+    def list_notes(self, limit=None):
+        """
+        List all the notes.
+        """
+        if self.notes is None:
+            self.notes = self.db.get_notes()
+        if limit is not None:
+            try:
+                if len(self.notes) > int(limit):
+                    # Print only up to the note limit.
+                    for note in self.notes[:limit]:
+                        print(note.id, note.content)
+                    # Set the remaining notes that haven't been displayed yet.
+                    self.notes = self.notes[limit:]
+                else:
+                    # Print all the notes.
+                    for note in self.notes:
+                        print(note.id, note.content)
+            except ValueError:
+                print("Invalid limit. Please enter a valid number.")
+                return
+        else:
+            # Print all the notes.
+            for note in self.notes:
+                print(note.id, note.content)
+                
+    def search_notes(self, query_string, limit=None):
+        """
+        Search notes for string matching query_string.
+        """
+        if self.s_notes is None or len(self.s_notes) < 1:
+            self.s_notes = self.db.get_notes()
+        
+        if limit is not None:
+            try:
+                if len(self.s_notes) > int(limit):
+                    # Print only up to the note limit.
+                    for note in self.s_notes[:limit]:
+                        print(note.id, note.content)
+                    # Set the remaining notes that haven't been displayed yet.
+                    self.s_notes = self.s_notes[limit:]
+                else:
+                    # Print all the notes.
+                    for note in self.s_notes:
+                        print(note.id, note.content)
+            except ValueError:
+                print("Invalid limit. Please enter a valid number.")
+                return
+        else:
+            # Print all the notes.
+            for note in self.s_notes:
+                print(note.id, note.content)
+                
+                    
+        
+    

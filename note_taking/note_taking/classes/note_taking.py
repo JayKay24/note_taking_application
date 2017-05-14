f2d3507ebc3f374b37ef4ea7c2c521312ee75518
James Kinyua <jameskinyua590@gmail.com>
2017-05-14 12:38:07 +0300

    Create a class NoteTaking

--- note_taking/note_taking/classes/note_taking.py
+++ note_taking/note_taking/classes/note_taking.py
@@ -4,4 +4,18 @@ Created on Sun May 14 09:21:07 2017
 
 @author: James Kinyua
 """
+import sys
+
 from note import Note
+
+# Add the classes directory path to sys to import classes.
+sys.path.append('../database')
+
+from db import NoteDB
+
+class NoteTaking:
+    def __init__(self):
+        self.db = NoteDB()
+        
+    def create_note(self, content):
+        note = Note(content)

cfbf2fbbaad265b41434ac0837bee4605c98ac22
James Kinyua <jameskinyua590@gmail.com>
2017-05-14 09:00:29 +0300

    Create a file note.py

---
+++ note_taking/note_taking/classes/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 14 08:59:35 2017
+
+@author: James Kinyua
+"""
+
---
+++ note_taking/note_taking/classes/note.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 14 08:59:58 2017
+
+@author: James Kinyua
+"""
+

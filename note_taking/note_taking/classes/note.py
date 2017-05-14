48c88e34a73ac70869d92c09a867129f082d5a83
James Kinyua <jameskinyua590@gmail.com>
2017-05-14 09:08:26 +0300

    Create a class Note

--- note_taking/note_taking/classes/note.py
+++ note_taking/note_taking/classes/note.py
@@ -4,4 +4,16 @@ Created on Sun May 14 08:59:58 2017
 
 @author: James Kinyua
 """
-
+class Note:
+    """
+    Class to create Note objects in the application note_taking
+    
+    :arg string content: Text assigned to the attribute content.
+    :arg int id: A number assigned to the attribute id.
+    
+    :ivar int id: A number to match the database note id.
+    :ivar string content: Text containing the note's content.
+    """
+    def __init__(self, content, id=None):
+        self.id = id
+        self.content = content
